ef8b4d7653f5d67e43924eb798fad868b3f572b5
jocarsa <info@josevicentecarratala.com>
2024-07-01 13:09:57 +0200

    10

--- blog/src/Articulos.js
+++ blog/src/Articulos.js
@@ -1,6 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
 
+function Articulo({titulo,contenido,fecha,categoria}){
+    return(
+        <div class="col-md-6">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+            <div class="col p-4 d-flex flex-column position-static">
+              <strong class="d-inline-block mb-2 text-primary-emphasis">{categoria}</strong>
+              <h3 class="mb-0">{titulo}</h3>
+              <div class="mb-1 text-body-secondary">{fecha}</div>
+              <p class="card-text mb-auto">{contenido}</p>
+              <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                Continue reading
+                <svg class="bi"></svg>
+              </a>
+            </div>
+            <div class="col-auto d-none d-lg-block">
+              <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            </div>
+          </div>
+        </div>   
+    )
+}
 
 function Articulos() {
     
@@ -14,23 +35,7 @@ function Articulos() {
     }, []);
     
     const mis_articulos = articulos.map(articulo => 
-        <div class="col-md-6">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-              <strong class="d-inline-block mb-2 text-primary-emphasis">{articulo.categoria}</strong>
-              <h3 class="mb-0">{articulo.titulo}</h3>
-              <div class="mb-1 text-body-secondary">{articulo.fecha}</div>
-              <p class="card-text mb-auto">{articulo.contenido}</p>
-              <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-                Continue reading
-                <svg class="bi"></svg>
-              </a>
-            </div>
-            <div class="col-auto d-none d-lg-block">
-              <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            </div>
-          </div>
-        </div>   
+        <Articulo titulo = {articulo.titulo} contenido={articulo.contenido}  fecha={articulo.fecha}  categoria={articulo.categoria} />
     )
 
   return (
